390a256c0d2dcb233d059f2bb54cab29cc4a47f5
AmmarNadeem <ammarpk2004@gmail.com>
2024-07-18 22:58:12 +0500

    Add sanitization,reqLimiter and security headers

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const hpp=require("hpp") // http paramter pollution
+const rateLimit=require("express-rate-limit") // limited no of req 
+const sanitize=require("express-mongo-sanitize") // sanitize for hacker attacks- nosql query
+const xss=require("xss-clean")  // sanitize for hacker attacks- html
+const helmet=require("helmet") //  security headers
+
 const productRouter = require("./Routes/productRoutes");
 const userRouter = require("./Routes/userRoutes");
 const orderRouter = require("./Routes/orderRoutes");
@@ -8,8 +14,28 @@ const messageRouter = require("./Routes/messageRoutes");
 const CustomError = require("./Utils/CustomError");
 const globalErrorHandler = require("./Controllers/errorControllers")
 const cors = require("cors");
+
+
 const app = express();
-app.use(express.json());
+
+
+
+app.use(helmet())
+let limtier=rateLimit({
+    max:3, // max no of req from particular ip
+    windowMs: 60*60*1000, // within time in ms
+    message:"We have received too many requests from this ip. Please try again later."
+})
+app.use("/api",limtier)
+
+app.use(express.json({limit:'10kb'}));
+
+app.use(sanitize())
+app.use(xss())
+// app.use(hpp({whitelist:''})) we can provide mustasna paramterers in whiteList
+
+
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("Public"));
 app.use(cors());
