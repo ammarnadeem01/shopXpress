a162088b65ea9ef9e344f6854149ded05d5143e6
AmmarNadeem <ammarpk2004@gmail.com>
2024-08-20 03:20:16 +0500

    change axiosConfig.js

--- Backend/app.js
+++ Backend/app.js
@@ -38,7 +38,7 @@ app.use(
     // origin: "http://localhost:5173",
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    // allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
